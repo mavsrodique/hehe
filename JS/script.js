f93ba9ba5e668cea6b82aa14fde28a0896ea9cd4
mavsrodique <mavigerodique@gmail.com>
2025-02-02 19:29:35 +0800

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+let leftPos = 0
+let bottomPos = 0
+document.getElementById('yessir').addEventListener('click', () => {
+    alert('You got yourself a date! To further discuss details about the date talk, to the creator of this invitation the most handsome and sweetest man on earth Mr.Mavige Rodique. ')
+})
+document.getElementById('nooo').addEventListener('mouseover', () => {
+    leftPos = Math.floor(Math.random() * (50 - (-50) + 1)) + (-50);
+    bottomPos = Math.floor(Math.random() * (100 - (-100) + 1)) + (-100);
+    document.getElementById('nooo').style.position = "absolute"
+    document.getElementById('nooo').style.left = leftPos + "%"
+    document.getElementById('nooo').style.bottom = bottomPos + "%"
+})
+document.getElementById('nooo').addEventListener('click', () => {
+    leftPos = Math.floor(Math.random() * (50 - (-50) + 1)) + (-50);
+    bottomPos = Math.floor(Math.random() * (100 - (-100) + 1)) + (-100);
+    document.getElementById('nooo').style.position = "absolute"
+    document.getElementById('nooo').style.left = leftPos + "%"
+    document.getElementById('nooo').style.bottom = bottomPos + "%"
+})
