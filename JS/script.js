17366e2ad863041acb9634b0627a532d3e2cad8b
mavsrodique <mavigerodique@gmail.com>
2025-02-03 10:56:37 +0800

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,7 @@
 let leftPos = 0
 let bottomPos = 0
 document.getElementById('yessir').addEventListener('click', () => {
-    alert('You got yourself a date! To further discuss details about the date talk, to the creator of this invitation the most handsome and sweetest man on earth Mr.Mavige Rodique. ')
+    alert('You got yourself a date! To further discuss details about the date,talk to the creator of this invitation. The most handsome and sweetest man on earth Mr.Mavige Rodique. ')
 })
 document.getElementById('nooo').addEventListener('mouseover', () => {
     leftPos = Math.floor(Math.random() * (50 - (-50) + 1)) + (-50);
